917c9bdce310a2cb44be3926eeb4a8e1bad400a9
StephenGregan <grego@live.ie>
2018-05-24 16:07:59 +0100

    initial commit

---
+++ ConsoleAppTitle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTitle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string progressBar = "Hello how are you today?";
+
+            var title = "";
+
+
+            for (int i = 0 ;i < progressBar.Length;i++)
+            {
+                title += progressBar[i];
+                Console.Title = title;
+                Thread.Sleep(100);
+            }
+
+            title = "";
+        }
+    }
+}
